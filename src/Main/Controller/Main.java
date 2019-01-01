c87456531b80ceccd5ecb08a6481d2644de67f01
DanialErfanian <erfaniiyanerfan@gmail.com>
2019-01-01 18:03:41 +0330

    set icon for application

--- src/Main/Controller/Main.java
+++ src/Main/Controller/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("../../resources/Views/FirstPage.fxml"));
         primaryStage.setTitle("DaniGram");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../../resources/images/icon.png")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
